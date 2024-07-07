20247b94547a5ddce40cb6402952e94a0f282126
Sherin S <62053384+sherinsk@users.noreply.github.com>
2024-07-07 16:18:19 +0530

    added something

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5174", // Update this to match your frontend URL
+    origin: "http://localhost:5174","https://chat-frontend-pied.vercel.app",// Update this to match your frontend URL
     methods: ["GET", "POST"]
   }
 });
