6a5774cc477d709baabc8be600eedb9f4bc25517
Correct Username <correct@example.com>
2024-07-10 11:08:32 +0530

    added

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["https://chat-frontend-pied.vercel.app", "http://localhost:5173"], // Added localhost
+    origin: ["https://chat-frontend-ashen.vercel.app", "http://localhost:5173"], // Added localhost
     methods: ["GET", "POST"]
   }
 });
